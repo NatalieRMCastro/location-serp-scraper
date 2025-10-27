576c36a1413677f1cacd171637ccc8cda3e032e5
Natalie Castro <134972160+NatalieRMCastro@users.noreply.github.com>
2025-10-27 15:22:53 -0400

    Update main.py
    trying to take a screenshot because I think it is hitting a captcha :(

--- main.py
+++ main.py
@@ -36,6 +36,12 @@ def main(query,canonical_name=False):
   if canonical_name == False:
     search_engine = ws.SearchEngine(selenium_config = s_config)
     search_engine.search(query)
+
+    ## Trying to screenshot if it is giving captcha - it did earlier
+    driver.save_screenshot("page.png")
+    with open("page.html", "w", encoding="utf-8") as f:
+        f.write(driver.page_source)
+    
     search_engine.parse_results()
     search_engine.save_serp(append_to='results-data/serp_tester.json')
     search_engine.save_results(append_to = 'results-data/serp_result_tester.json')
